bbf1b8c2dfd927f78ae23eb2db8b78427026c988
Krzysztof Zdulski <krzys.zdulski@gmail.com>
2021-03-11 12:32:15 +0100

    Implement syscall table tamper detection

--- module/anti_rootkit_lkm.c
+++ module/anti_rootkit_lkm.c
@@ -1,5 +1,7 @@
-#include "asm-generic/fcntl.h"
+#include "linux/gfp.h"
 #include "linux/kern_levels.h"
+#include "linux/list.h"
+#include "linux/mm.h"
 #include "linux/printk.h"
 #include "linux/string.h"
 #include "linux/types.h"
@@ -22,37 +24,29 @@ MODULE_AUTHOR("Krzysztof Zdulski");
 MODULE_DESCRIPTION("Simple anti-rookit module for a security course.");
 MODULE_VERSION("0.0.1");
 
-void *find_virt_start(void) {
-    return phys_to_virt(0);
+struct syscall_overwrite {
+    unsigned int nr;
+    void *original_addr;
+    void *overwritten_addr;
+    struct list_head list;
+};
+
+static void *syscall_table_cpy[NR_syscalls];
+static void **syscall_table;
+
+static inline void _write_cr0(uint64_t val) {
+    asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
 }
 
-void *find_start_symbol(void) {
-    unsigned long addr = 0xffffffff80000000;
-    char buf[256];
-
-    while(sprint_symbol(buf, addr) < 20 && addr < 0xffffffffffffffff) {
-        addr += 0x100;
-    }
-
-    printk(KERN_INFO "We found some symbols, %s @ %px", buf, (void *) addr);
-
-    return (void *) (addr & 0xfffffffffff00000);
+static inline void disable_wp(void) {
+     _write_cr0(read_cr0() & (~ 0x10000));
 }
 
-/* static inline */
-/* u8* get_32bit_system_call_handler(void) */
-/* { */
-/* 	struct desc_ptr interrupt_descriptor_table; */
-/* 	gate_desc* interrupt_gates; */
+static inline void enable_wp(void) {
+     _write_cr0(read_cr0() | 0x10000);
+}
 
-/* 	store_idt(&interrupt_descriptor_table); */
-/* 	interrupt_gates = (gate_desc*) interrupt_descriptor_table.address; */
-
-/* 	return (u8*) gate_offset(interrupt_gates[IA32_SYSCALL_VECTOR]); */
-/* } */
-
-static inline
-void *get_64bit_system_call_handler(void)
+static inline void *get_64bit_system_call_handler(void)
 {
 	uint64_t system_call_entry;
 	rdmsrl(MSR_LSTAR, system_call_entry);
@@ -61,12 +55,12 @@ void *get_64bit_system_call_handler(void)
 
 #define ENTRY_DO_CALL_OFFSET 0x77
 
-void **find_syscall_table(void) {
-    uint8_t *entry_syscall;
-    uint8_t *do_syscall;
+static void **find_syscall_table(void) {
+    unsigned char *entry_syscall;
+    unsigned char *do_syscall;
+    unsigned char *addr;
     void **syscall_table = NULL;
-    uint8_t *addr;
-    int32_t offset;
+    int offset;
 
     entry_syscall = get_64bit_system_call_handler();
 
@@ -80,7 +74,7 @@ void **find_syscall_table(void) {
     for(addr = do_syscall; addr < do_syscall + 0xff; ++addr) {
         if(addr[0] == 0x48 && addr[1] == 0x8b && addr[2] == 0x04 && addr[3] == 0xc5) {
             offset = *((int *) (addr + 4));
-            printk(KERN_INFO "offset : %d", offset);
+            // Sign extend
             syscall_table = (void **) (offset < 0 ? 0xffffffff00000000 | offset : offset);
             break;
         }
@@ -89,13 +83,112 @@ void **find_syscall_table(void) {
     return syscall_table;
 }
 
+void copy_syscall_table(void) {
+    memcpy(syscall_table_cpy, syscall_table, sizeof(syscall_table_cpy));
+}
+
+static void recover_syscall_table(struct syscall_overwrite *head) {
+    struct syscall_overwrite *ov;
+
+    printk(KERN_INFO "Recovering syscall table");
+    list_for_each_entry(ov, &head->list, list) {
+        disable_wp();
+        syscall_table[ov->nr] = ov->original_addr;
+        enable_wp();
+    }
+
+}
+
+static struct syscall_overwrite *syscall_overwrites(void) {
+    unsigned int nr;
+    struct syscall_overwrite *head = kmalloc(sizeof(*head), GFP_KERNEL);
+    INIT_LIST_HEAD(&head->list);
+
+    if(head == NULL) {
+        return NULL;
+    }
+
+    for(nr = 0; nr < NR_syscalls; ++nr) {
+        if(syscall_table_cpy[nr] != syscall_table[nr]) {
+            struct syscall_overwrite *ov = 
+                kmalloc(sizeof(*ov), GFP_KERNEL);
+            if(ov == NULL) {
+                return NULL;
+            }
+
+            ov->nr = nr;
+            ov->original_addr = syscall_table_cpy[nr];
+            ov->overwritten_addr = syscall_table[nr];
+            list_add(&ov->list, &head->list);
+        }
+    }
+
+    return head;
+}
+
+static void free_syscall_overwrites(struct syscall_overwrite *head) {
+    struct list_head *cur, *tmp;
+    struct syscall_overwrite *ov;
+
+    list_for_each_safe(cur, tmp, &head->list) {
+        ov = list_entry(cur, struct syscall_overwrite, list);
+        list_del(cur);
+        kfree(ov);
+    }
+
+    kfree(head);
+}
+
+static void print_syscall_overwrites(struct syscall_overwrite *head) {
+    struct syscall_overwrite *ov;
+
+    if(list_empty(&head->list)) {
+        printk(KERN_INFO "No overwrites detected");
+        return;
+    }
+
+    list_for_each_entry(ov, &head->list, list) {
+        printk(KERN_WARNING "syscall %d changed, used to be %px, now is %px", 
+                ov->nr, ov->original_addr, ov->overwritten_addr);
+    }
+}
+
 static int __init anti_rootkit_init(void) {
-    void **syscall_table;
+    struct syscall_overwrite *head;
 
     printk(KERN_INFO "Loading anti-rootkit module");
 
     syscall_table = find_syscall_table();
+
+    if(syscall_table == NULL) {
+        return 1;
+    }
+
     printk(KERN_INFO "syscall_table is @ %px", syscall_table);
+
+    copy_syscall_table();
+    
+    disable_wp();
+    syscall_table[300] = (void *) 0xdeafbeef;
+    syscall_table[323] = (void *) 0x12345678;
+    enable_wp();
+
+    head = syscall_overwrites();
+    if(head == NULL) {
+        return 1;
+    }
+
+    print_syscall_overwrites(head);
+    recover_syscall_table(head);
+    free_syscall_overwrites(head);
+    
+    head = syscall_overwrites();
+    if(head == NULL) {
+        return 1;
+    }
+
+    print_syscall_overwrites(head);
+    free_syscall_overwrites(head);
     
     printk(KERN_INFO "Done");
 
